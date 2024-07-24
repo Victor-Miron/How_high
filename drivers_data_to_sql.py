a6d44d55de23308ad6a992a8af813ddc965ecdee
Victor Miron <victormiron.home@gmail.com>
2024-07-23 19:17:55 -0700

    updates

---
+++ drivers_data_to_sql.py
@@ -0,0 +1,15 @@
+import sqlite3
+import pandas as pd
+
+# Connect Database to sqlite
+
+conn = sqlite3.connect("Greatest.db")
+
+# Load data into a panda Dataframe
+df = pd.read_sql_query("SELECT * FROM 'Drivers Data'", conn)
+
+# Close conn
+conn.close()
+
+# Save data to a CSV file
+df.to_csv('datas_for_tableau.csv', index=False)
