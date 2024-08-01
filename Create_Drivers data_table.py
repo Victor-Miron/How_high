5b299f4a30532f8585e52d3751fe676e633b09ee
Victor Miron <victormiron.home@gmail.com>
2024-07-31 21:34:00 -0400

    Fixed the problem with the cleaning. The problem was not the connection, it was the csv files which were missing some data

--- Create_Drivers data_table.py
+++ Create_Drivers data_table.py
@@ -12,57 +12,60 @@ conn.execute('BEGIN TRANSACTION;')
 try:
     # Step 1: Create the initial table with the primary keys
     cur.execute('''
-        CREATE TABLE IF NOT EXISTS Drivers_Data (
+        CREATE TABLE IF NOT EXISTS "Drivers Data" (
             driverId INTEGER,
             raceId INTEGER,
-            resultId INTEGER
+            resultId Integer
         )
     ''')
 
     # Step 2: Populate the table with the primary keys
     cur.execute('''
-        INSERT INTO Drivers_Data (driverId, raceId, resultId)
+        INSERT INTO "Drivers Data" (driverId, raceId, resultId)
         SELECT DISTINCT driverId, raceId, resultId
-        FROM Results
+        FROM "Results"
     ''')
 
     # Step 3: Add columns from Drivers
-    cur.execute('ALTER TABLE Drivers_Data ADD COLUMN Forename TEXT')
-    cur.execute('ALTER TABLE Drivers_Data ADD COLUMN Surname TEXT')
-    cur.execute('ALTER TABLE Drivers_Data ADD COLUMN Nationality TEXT')
+    cur.execute('ALTER TABLE "Drivers Data" ADD COLUMN Forename TEXT')
+    cur.execute('ALTER TABLE "Drivers Data" ADD COLUMN Surname TEXT')
+    cur.execute('ALTER TABLE "Drivers Data" ADD COLUMN Nationality TEXT')
     cur.execute('''
-        UPDATE Drivers_Data
+        UPDATE "Drivers Data"
         SET Forename = (SELECT forename FROM Drivers
-                WHERE Drivers.driverId = Drivers_Data.driverId),
+                WHERE Drivers.driverId = "Drivers Data".driverId),
             Surname = (SELECT surname FROM Drivers
-                WHERE Drivers.driverId = Drivers_Data.driverId),
+                WHERE Drivers.driverId = "Drivers Data".driverId),
             Nationality = (SELECT nationality FROM Drivers 
-                WHERE Drivers.driverId = Drivers_Data.driverId)
+                WHERE Drivers.driverId = "Drivers Data".driverId)
     ''')
 
-    # Step 4: Add columns from Results and Races
-    cur.execute('ALTER TABLE Drivers_Data ADD COLUMN Year INTEGER')
-    cur.execute('ALTER TABLE Drivers_Data ADD COLUMN Circuit_Name TEXT')
-    cur.execute('ALTER TABLE Drivers_Data ADD COLUMN Points INTEGER')
-    cur.execute('ALTER TABLE Drivers_Data ADD COLUMN Time TEXT')
-    cur.execute('ALTER TABLE Drivers_Data ADD COLUMN Rank INTEGER')
-    cur.execute('ALTER TABLE Drivers_Data ADD COLUMN FastestLapTime TEXT')
+    # Step 4: Add columns from TempResults
+    cur.execute('ALTER TABLE "Drivers Data" ADD COLUMN Year INTEGER')
+    cur.execute('ALTER TABLE "Drivers Data" ADD COLUMN "Circuit Name" TEXT')
+    cur.execute('ALTER TABLE "Drivers Data" ADD COLUMN Points INTEGER')
+    cur.execute('ALTER TABLE "Drivers Data" ADD COLUMN Rank INTEGER')
+    cur.execute('ALTER TABLE "Drivers Data" ADD COLUMN FastestLapTime TEXT')
     cur.execute('''
-        UPDATE Drivers_Data
-        SET Year = (SELECT year FROM Races 
-                WHERE Races.raceId = Drivers_Data.raceId),
-            Circuit_Name = (SELECT name FROM Circuits
-                WHERE Circuits.circuitId = (SELECT circuitId FROM Races WHERE Races.raceId = Drivers_Data.raceId)), 
+        UPDATE "Drivers Data"
+        SET Year = (SELECT year FROM "Races" 
+                WHERE Races.raceId = "Drivers Data".raceId),
+            "Circuit Name" = (SELECT name FROM "Races" 
+                WHERE Races.raceId = "Drivers Data".raceId), 
             Points = (SELECT points FROM Results 
-                WHERE Results.resultId = Drivers_Data.resultId),
-            Time = (SELECT time FROM Results 
-                WHERE Results.resultId = Drivers_Data.resultId),
+                WHERE Results.resultId = "Drivers Data".resultId),
             Rank= (SELECT rank FROM Results 
-                WHERE Results.resultId = Drivers_Data.resultId),
+                WHERE Results.resultId = "Drivers Data".resultId),
             FastestLapTime = (SELECT fastestLapTime FROM Results 
-                WHERE Results.resultId = Drivers_Data.resultId)
+                WHERE Results.resultId = "Drivers Data".resultId)
     ''')
-
+    cur.execute('''
+        UPDATE "Drivers Data"
+        SET 
+        Rank = REPLACE(Rank, '\\N', 0),
+        FastestLapTime = REPLACE(FastestLapTime, '\\N', 0)
+        WHERE Rank LIKE '%\\N%' OR FastestLapTime LIKE '%\\N%';
+''')
     # Commit the transaction
     conn.commit()
 except Exception as e:
